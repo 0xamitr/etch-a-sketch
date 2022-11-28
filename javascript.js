1ff87ab5f624c5b239045a6497df38a8cf9456af
amit-rathore0 <amitrathore.amit.ar.70@gmail.com>
2022-11-28 19:11:25 +0530

    added multiple colors

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
 let number;
 const container = document.querySelector(".container");
+function rgb0(){
+    let rgb1 = Math.floor(Math.random() * 256);
+    let rgb2 = Math.floor(Math.random() * 256);
+    let rgb3 = Math.floor(Math.random() * 256);
+    return [rgb1, rgb2, rgb3];
+}
 function promptfunc(){
     number = prompt("No of squares you want on a side");
     if (number > 100){
         alert("number should be less than 100");
         promptfunc();
     }
-    let num = (800 ) / number;
+    let num = (800 - 4 * number) / number;
     for (let i = 0; i < number; i++){
         const div = document.createElement('div');
         div.setAttribute("style", "display: flex;");
@@ -17,16 +23,21 @@ function promptfunc(){
             div1.setAttribute("style", `height: ${num}px; width: ${num}px;`)
             div.appendChild(div1);
         }
+    }
         const div2 = document.querySelectorAll(".square");
         div2.forEach(element => {
-            element.addEventListener("mouseover" , () => {
-                element.style["background-color"] = "black";
+            let i = 0;
+            let rgbc = rgb0();
+            let a = rgbc[0];
+            let b = rgbc[1];
+            let c = rgbc[2];
+            element.addEventListener("mouseenter" , () => {
+                element.style["background-color"] = 'rgb(' + (a - ((a/10)*i)) + ',' + (b - ((b/10)*i)) + ',' + (c - ((c/10)*i)) + ')';
+                i++;
             });
-        });
-    }
+    });
 }
 promptfunc();
-
 function newpromptfunc(){
     while (container.firstChild) {
         while (container.firstChild.firstChild){
